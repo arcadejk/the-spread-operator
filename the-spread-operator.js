98613bb1722eceb7a12406eea6461be1e9d997cd
Arcade Kakpo <arcadej@hotmail.fr>
2024-06-25 20:55:55 +0000

    Create a spread operator adding dots

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -6,9 +6,18 @@
 // No spread operator
 let arr1 = [1, 2, 3];
 let arr2 = arr1;
+
+arr2.push(4)
+console.log("First array:", arr1);
 console.log("Second array:", arr2);
 
 // Copying an array
+let arr3 = [3, 4, 6];
+let arr4 = [...arr3];
+
+arr4.push(7);
+console.log("third array:", arr3);
+console.log("fourth array:", arr4);
 
 // Copying an object
 
